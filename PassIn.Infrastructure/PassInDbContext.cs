937602efdb817fe322a2eb0e1c85df6fbf5ee96e
Lucas <Lucas@LLAWLIETB2>
2025-07-21 21:13:06 -0300

    Adicionar arquivos de projeto.

---
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Infrastructure
+{
+    public class PassInDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; } = null!;   
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source =E:\\Lucas\\Projeto de Estudo\\PassInDb.db ");
+        }
+    }
+}
